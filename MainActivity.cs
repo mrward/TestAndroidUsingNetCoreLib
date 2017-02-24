8122b7f86a75694761e68adfd32dc96540f35407
Matt Ward <matt.ward@xamarin.com>
2017-02-24 15:17:37 +0000

    Initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using NetStandardLib;
+
+namespace AndroidUsingNetCoreLibTest
+{
+	[Activity(Label = "AndroidUsingNetCoreLibTest", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			button.Text = new Class1().Message;
+
+			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+		}
+	}
+}
+
---
+++ NetStandardLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NetStandardLib
+{
+    public class Class1
+    {
+		public string Message {
+			get { return "Hello from .NET Standard library"; }
+		}
+    }
+}
